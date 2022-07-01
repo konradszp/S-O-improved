778e70025568a5735b3f67e9627203934735bc22
konradszp <101819128+konradszp@users.noreply.github.com>
2022-07-01 12:52:39 +0100

    Initial update
    Creating new classes for player, enemy and merchant. Implementing same Game and GameFrame from the previous project. GamePanel will be different due to new objects implementation.

---
+++ GamePanel.java
@@ -0,0 +1,14 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+import java.util.ArrayList;
+
+public class GamePanel extends JPanel implements ActionListener {
+    static final int SCREEN_WIDTH = 1300;
+	static final int SCREEN_HEIGHT = 750;
+	static final int UNIT_SIZE = 50;
+	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE);
+
+    
+}
